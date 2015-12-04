b9f3c33293d45c9c6a96ee0a9cf8cefe0b942ddb
Sachin Vas <SachinVas@air-watch.com>
2015-12-04 20:59:56 +0530

    changes

---
+++ FIR/FIR/TabbarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabbarViewController.h
+//  FIR
+//
+//  Created by Sachin Vas on 12/4/15.
+//  Copyright © 2015 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbarViewController : UITabBarController
+
+@end
---
+++ FIR/FIR/TabbarViewController.m
@@ -0,0 +1,46 @@
+//
+//  TabbarViewController.m
+//  FIR
+//
+//  Created by Sachin Vas on 12/4/15.
+//  Copyright © 2015 Vijay. All rights reserved.
+//
+
+#import "TabbarViewController.h"
+
+@interface TabbarViewController ()
+
+@end
+
+@implementation TabbarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSArray *tabBarImages = @[@"Filing", @"", @"Emergency"];
+    
+    for (int i = 0; i < 3; i++) {
+        NSString *imageName = tabBarImages[i];
+        self.tabBar.items[i].image = [UIImage imageNamed:imageName];
+        self.tabBar.items[i].selectedImage = [UIImage imageNamed:imageName];
+    }
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
