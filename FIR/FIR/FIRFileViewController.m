60e45773806eaa93d4de072886ff1832d3aec55c
Sachin Vas <SachinVas@air-watch.com>
2015-12-08 14:11:17 +0530

    new ui changes

--- FIR/FIR/FIRFileViewController.m
+++ FIR/FIR/FIRFileViewController.m
@@ -58,7 +58,7 @@
     self.textView.layer.borderColor = borderColor.CGColor;
     self.textView.layer.borderWidth = 1.0;
     self.textView.layer.cornerRadius = 5.0;
-    self.textView.placeholder = @"Please provide us some information about accident.";
+    self.textView.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Please provide us some information about accident." attributes:@{NSForegroundColorAttributeName : [UIColor colorWithRed:(110.0/255.0) green:(110.0/255.0) blue:(199.0/255.0) alpha:1.0f]}];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
