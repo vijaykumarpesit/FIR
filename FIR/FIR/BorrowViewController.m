6e8e8defa9d10fb075a074554b48dda2822dfe4b
Sachin Vas <sachinvas16@gmail.com>
2017-05-20 16:13:09 +0530

    New ViewC changes.

---
+++ FIR/FIR/BorrowViewController.m
@@ -0,0 +1,61 @@
+//
+//  BorrowViewController.m
+//  FIR
+//
+//  Created by Sachin Vas on 20/05/17.
+//  Copyright © 2017 Vijay. All rights reserved.
+//
+
+#import "BorrowViewController.h"
+
+@interface BorrowViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITextField *amountTextField;
+@property (weak, nonatomic) IBOutlet UISlider *amountSlider;
+@property (weak, nonatomic) IBOutlet UITextField *daysTextField;
+@property (weak, nonatomic) IBOutlet UISlider *daysSlider;
+@property (weak, nonatomic) IBOutlet UITableView *documentsView;
+@end
+
+@implementation BorrowViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)submitTapped:(id)sender {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Document"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Document"];
+    }
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
